bb2628d10390e9aa7aac89c29e08d0de32ec0bb8
dengshaojie <506161132@qq.com>
2020-01-21 14:46:07 +0800

    做好demo

--- generator-web/hello-world/src/App.vue
+++ generator-web/hello-world/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <h1>Hello App!</h1>
+    <router-link to="/helloWorld">
+      <el-button>go hello world</el-button>
+    </router-link>
+    <router-link to="/login">
+      <el-button>go login page</el-button>
+    </router-link>
+    <router-link to="/">
+      <el-button>go home</el-button>
+    </router-link>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import VueRouter from "vue-router";
+
+export default {
+  name: "app",
+  VueRouter,
+  components: {
+
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +33,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- generator-web/hello-world/src/router/index.js
+++ generator-web/hello-world/src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
+import home from "../views/home";
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
     name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/",
+    component: home
+  }, {
+    name: "helloWorld",
+    path: "/helloWorld",
+    component: () => import("../views/hello-world")
+  }, {
+    name: "login",
+    path: "/login",
+    component: () => import("../views/login")
   }
 ];
 
