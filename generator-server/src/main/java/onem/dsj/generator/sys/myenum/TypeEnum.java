7cf3d96e5cb333a704aad37fa8958502fd5dc27c
dengshaojie <506161132@qq.com>
2020-03-10 18:56:45 +0800

    为类型枚举增加一个str文字说明

--- generator-server/src/main/java/onem/dsj/generator/sys/myenum/TypeEnum.java
+++ generator-server/src/main/java/onem/dsj/generator/sys/myenum/TypeEnum.java
@@ -1,5 +1,6 @@
 package onem.dsj.generator.sys.myenum;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.StringUtils;
@@ -9,16 +10,20 @@ import org.springframework.util.StringUtils;
  * @date 2020/1/17 18:45
  */
 @NoArgsConstructor
+@AllArgsConstructor
 public enum TypeEnum {
-    string(1, "nvarchar2(2000)"),
-    number(2, "number"),
-    whether(3, "char(10)");
+    string(1, "nvarchar2(2000)", "字符串"),
+    number(2, "number(10)", "数字"),
+    whether(3, "number(10)", "是否");
 
     @Getter
     private Integer code;
 
     @Getter
     private String name;
+
+    @Getter
+    private String str;
 
     TypeEnum(Integer code, String name) {
         this.code = code;
